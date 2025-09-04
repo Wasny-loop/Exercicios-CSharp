00fb7f896c3c1c7281832f0689104cc37ccc6125
João Wasny <125569896+Wasny-loop@users.noreply.github.com>
2025-09-04 01:50:36 -0300

    Add files via upload

---
+++ Exercicio02/Exercicio02/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double largura, comprimento, valor, area = 0, preco = 0;
+
+            largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            comprimento = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            valor = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            area = largura * comprimento;
+            preco = valor * area;
+
+            Console.WriteLine("AREA = " + area.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("PRECO = " + preco.ToString("F2", CultureInfo.InvariantCulture));
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Exercicio03/Exercicio03/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double BaseRetangulo, AlturaRetangulo, area, perimetro, diagonal;
+
+            BaseRetangulo = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            AlturaRetangulo = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            area = BaseRetangulo * AlturaRetangulo;
+            perimetro = (2 * BaseRetangulo) + (2 * AlturaRetangulo);
+            diagonal = Math.Sqrt(Math.Pow(BaseRetangulo, 2) + Math.Pow(AlturaRetangulo, 2));
+
+            Console.WriteLine("AREA = " + area.ToString("F4", CultureInfo.InvariantCulture));
+            Console.WriteLine("PERIMETRO = " + perimetro.ToString("F4", CultureInfo.InvariantCulture));
+            Console.WriteLine("DIAGONAL = " + diagonal.ToString("F4", CultureInfo.InvariantCulture));
+
+            Console.ReadLine();
+        }
+    }
+}
